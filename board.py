9ece459945bde0782cf7192266934a84325c34d7
Aleksej10 <37964057+Aleksej10@users.noreply.github.com>
2019-01-16 21:02:50 +0100

    Add files via upload

--- board.py
+++ board.py
@@ -4,18 +4,23 @@ from numpy import array
 
 
 class Board():
+    #static list lines; each line on the board is represented as '1' in a long binary number
+    #each element of lines is a decimal representation of a single line on the board
     lines = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 281474976710656, 562949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984, 36028797018963968, 72057594037927936, 144115188075855872, 288230376151711744, 576460752303423488, 1152921504606846976, 2305843009213693952, 4611686018427387904, 9223372036854775808, 18446744073709551616, 36893488147419103232, 73786976294838206464, 147573952589676412928, 295147905179352825856, 590295810358705651712, 1180591620717411303424, 2361183241434822606848, 4722366482869645213696, 9444732965739290427392, 18889465931478580854784, 37778931862957161709568, 75557863725914323419136, 151115727451828646838272, 302231454903657293676544, 604462909807314587353088, 1208925819614629174706176, 2417851639229258349412352, 4835703278458516698824704, 9671406556917033397649408, 19342813113834066795298816, 38685626227668133590597632, 77371252455336267181195264, 154742504910672534362390528, 309485009821345068724781056, 618970019642690137449562112, 1237940039285380274899124224, 2475880078570760549798248448, 4951760157141521099596496896, 9903520314283042199192993792, 19807040628566084398385987584, 39614081257132168796771975168]
     def __init__(self, linije, scr, side):
-        self.linije = linije
-        self.side = side
-        self.points = 57
+        self.linije = linije        #binary number holding lines
+        self.side = side            #side to move, '1' or '-1'
+        self.points = 57            #at the start there are 57 squares to be marked as either 'x' or 'o'
+        #squares are for checking points
+        #each element of self.squares is a decimal number representing
+        #                        all the lines needed to make a square
         self.squares = [5, 11, 18, 544, 1124, 2248, 4496, 8448, 1064960, 2146816, 4293632, 8587264, 17174528, 34349056, 67633152, 34493956096, 69123178496, 138246356992, 276492713984, 552985427968, 1105970855936, 2211941711872, 4423883423744, 8813272891392, 4521226173153280, 9060044532350976, 18120089064701952, 36240178129403904, 72480356258807808, 144960712517615616, 289921425035231232, 579842850070462464, 1155182100513554432, 2310346608841064448, 597222346585601474560, 1194444693171202949120, 2388889386342405898240, 4777778772684811796480, 9555557545369623592960, 19111115090739247185920, 38222230181478494371840, 296300826683959672832, 76148159536273029070848, 5063557461256977080385536, 10127114922513954160771072, 20254229845027908321542144, 40508459690055816643084288, 81016919380111633286168576, 2455630571092215511121920, 159578208189131051061215232, 2949778999859695186283069440, 5899557999719390372566138880, 11799115999438780745132277760, 1315311291740716542080319488, 22282920707136844948184236032, 64372882042839774294754459648, 49517601571415210995964968960]
-        self.scr = scr
+        self.scr = scr              #difference in points
 
     def done(self):
-        if self.linije == 79228162514264337593543950335:
+        if self.linije == 79228162514264337593543950335: # <---big number is just all lines marked on the board
             return True
-        if abs(self.scr) > self.points:
+        if abs(self.scr) > self.points:  #game if over if difference is greater than remaining points
             return True
         return False
 
@@ -28,13 +33,51 @@ class Board():
 #            moves.remove(move)
 #        return 1 - 2*(tmp.score()<0)
 #
-#    def sim(self, n):
+#    def sim(self, n):                 #simulates gameplay n times and returns average
 #        sm = 0
 #        for i in range(n):
 #            sm += self.simulate()
 #        return 1.0*sm/n
 
-    def printLines(self):
+    def getMoves(self):                 #returns a list of all non-marked lines --> all possible moves
+        moves = []
+        for line in self.lines:
+            if (line & self.linije) == 0:
+                moves.append(line)
+        return moves
+
+    def playMove(self, move):
+        self.linije |= move             #marks line on the board
+        k = 0
+        for i in range(len(self.squares)-1, -1, -1): #check if the added line formed a square (or two)
+            if k>1:
+                break
+            if (self.squares[i] & self.linije) == self.squares[i]: #if it did, there is one less point in the game
+                self.points -= 1
+                k+=1
+                if self.side == 1:
+                    self.scr += 1
+                else:
+                    self.scr -= 1
+                self.squares.pop(i)          #once the square is makred, it is removed from the list
+        if k == 0:
+            self.side *= -1                  #if players marked a square he has another turn
+
+    def getNumpyArray(self):                 #creates numpy array which is used as an input to a neural network
+        nar = []                             #first 4 elements are side, difference in points, number of lines left
+        nar.append(self.side)                                                            #and number of points left
+        nar.append(self.scr)                 #other 96 elements represent lines, '1' for marked line and '0' otherwise
+        nar.append(96 - bin(self.linije).count('1'))
+        nar.append(self.points)
+        for line in self.lines:
+            if (self.linije & line) > 0:
+                nar.append(1)
+            else:
+                nar.append(0)
+        return nar
+
+
+    def printLines(self): #an attempt of visual representation of the board
         k = 0;
         for i in range(8):
             space = ' ' * (7-i)
@@ -63,45 +106,7 @@ class Board():
                     print('0 ', end='')
                 k+=1
             print()
-
-    def getMoves(self):
-        moves = []
-        for line in self.lines:
-            if (line & self.linije) == 0:
-                moves.append(line)
-        return moves
-
-    def playMove(self, move):
-        self.linije |= move
-        k = 0
-        for i in range(len(self.squares)-1, -1, -1):
-            if k>1:
-                break
-            if (self.squares[i] & self.linije) == self.squares[i]:
-                self.points -= 1
-                k+=1
-                if self.side == 1:
-                    self.scr += 1
-                else:
-                    self.scr -= 1
-                self.squares.pop(i)
-        if k == 0:
-            self.side *= -1
-
-    def getNumpyArray(self):
-        nar = []
-        nar.append(self.side)
-        nar.append(self.scr)
-        nar.append(96 - bin(self.linije).count('1'))
-        nar.append(self.points)
-        for line in self.lines:
-            if (self.linije & line) > 0:
-                nar.append(1)
-            else:
-                nar.append(0)
-        return array(nar).reshape(1,100)
-
-
+        print()
 
 
 
--- node.py
+++ node.py
@@ -9,52 +9,58 @@ from keras.layers import Dense
 
 class Node():
     try:
-        model = load_model('model')
+        model = load_model('model')             #tries to load static model of size [100, 50, 20, 1]
         print('model loaded from file')
     except OSError:
         model = Sequential()
-        model.add(Dense(50, input_dim=100, activation='relu'))
-        model.add(Dense(20, activation='relu'))
-        model.add(Dense(1, activation='tanh'))
-        model.compile(loss='mean_absolute_percentage_error', optimizer='adam', metrics=['accuracy'])
+        model.add(Dense(50, input_dim=100, activation='tanh'))
+        model.add(Dense(20, activation='tanh'))
+        model.add(Dense(1, activation='tanh'))  #output layer represents probability for winning, from -1 to 1
+        model.compile(loss='mse', optimizer='rmsprop')
         model.save('model')
 
+    #training data
+    data_in = []
+    exp_data = []
+
     def __init__(self, b, father):
-        self.b = b
-        self.father = father
-        self.sons = []
-        self.visits = 0
-        self.score = 0.0
-        self.true_eval = 0
-        self.done = 0
-        self.estimate = 0.0
-        self.inn = b.getNumpyArray()
+        self.b = b              #board
+        self.father = father    #father node (for backpropagation)
+        self.sons = []          #list of sons (basicaly paths to all possible position from current one)
+        self.visits = 0         #number of visits, used when calculating usb1
+        self.score = 0.0        #summed probabilities of winning
+        self.true_eval = 0      #true eval for leaf nodes or nodes fairly close to them
+        self.done = 0           #node can be done without it's board being done, i just means it's true value is determined, no need to look at it any longer
+        self.estimate = 0.0     #neural networks estimation, used for updating the weights later
+        self.inn = array(self.b.getNumpyArray()).reshape(1,100)     #input to a neural network
 
     def expand(self):
-        moves = self.b.getMoves()
+        moves = self.b.getMoves()       #connects node to all possible positions (nodes) available from current state
         for move in moves:
             tmp = deepcopy(self.b)
             tmp.playMove(move)
             self.sons.append(Node(tmp, self))
 
-    def show(self):
+    def show(self):                     #visual representation of a node
         print('visits: ', end='')
         print(self.visits, end=' ')
         print('chances: ', end='')
         print(f64(self.score)/self.visits, end=' ')
-        print('true_eval: ', end='')
-        print(self.true_eval, end=' ')
         print('done: ', end='')
-        print(self.done)
+        print(self.done, end=' ')
+        print('estimate: ', end='')
+        print(self.estimate)
 
-    def showSons(self):
+    def showSons(self):                 #family display
         for son in self.sons:
             son.show()
 
-    def usb1(self):
-        return (f64(self.score)/self.visits) + e*sqrt(f64(log(self.father.visits))/self.visits)
+    def usb1(self):                     #how you pick which path to pay a visit
+        if self.visits == 0:            #state of art balance between exploration and exploitation
+            return -inf * self.b.side
+        return (f64(self.score)/self.visits) - self.b.side*e*sqrt(2 * f64(log(self.father.visits))/self.visits)
 
-    def backprop(self, score, done):
+    def backprop(self, score, done): #completely intuitive process of backpropagation
         self.score += score
         self.visits += 1
         self.done = done
@@ -66,51 +72,57 @@ class Node():
 
     def monte(self):
         if len(self.sons) == 0: #leaf node
-            if self.b.done():
-                ev = 1 if (self.b.scr > 0) else -1
-                self.backprop(ev, 1)
+            if self.b.done():                           #game is finished
+                ev = 1 if (self.b.scr > 0) else -1      #get eval and backpropagate
+                self.estimate = self.model.predict(self.inn)[0,0]  #also save neural network's estimation
+                self.backprop(ev, 1)                               #          for fitting the model later
                 return
-            if self.visits == 0:
-                est = self.model.predict(self.inn)[0,0]
-                self.estimate = est
-                self.backprop(est, 0)
-                return
+            if self.visits == 0:                        #get estimate and backpropagete, if the estimation is good
+                self.estimate = self.model.predict(self.inn)[0,0]        #the node will be visited again soon,
+                self.backprop(self.estimate, 0)                          #and then self.visits won't be zero, so
+                return                                                   #the node is getting expanded
             else:
                 self.expand()
                 return
         else:
-            usbs = []
+            usbs = []       #here we choose where to go from current node
             k = 0
             for son in self.sons:
                 if son.done:
-                    usbs.append(-inf*self.b.side)
+                    usbs.append(-inf*self.b.side)   #we don't want to explore nodes we already know true eval
                     k+=1
                 else:
                     usbs.append(son.usb1())
             if k>0:
-                truz = [son.true_eval for son in self.sons]
+                truz = [son.true_eval for son in self.sons]    #true_eval can be either 1 or -1
                 if self.b.side == 1:
-                    if 1 in truz:
-                        self.backprop(1,1)
+                    if 1 in truz:               #if '1' is to play, and he can win a game with that move,
+                        self.backprop(1,1)      #it's node is being declared done and it's true value is being updated accordingly
                         return
                     else:
-                        if len(usbs) == k:
-                            self.backprop(-1,1)
+                        if len(usbs) == k:      #if every node on 1's turn is done, and he is losing no matter what he plays,
+                            self.backprop(-1,1) #it's node is being declared done and it's true value is being updated accordingly
                             return
                 else:
-                    if -1 in truz:
+                    if -1 in truz:              #same stuff, only if '-1' is to play
                         self.backprop(-1,1)
                         return
                     else:
                         if len(usbs) == k:
                             self.backprop(1,1)
                             return
+            #finaly, if node is not done, we chose where to go
             if self.b.side == 1:
                 self.sons[argmax(usbs)].monte()
             else:
                 self.sons[argmin(usbs)].monte()
 
-    def chose(self):
+    def mcts(self, n):                  #just runs monte n times
+        while (not self.done) and (n>0):
+            self.monte()
+            n-=1
+
+    def chose(self):                #basicaly min/max
         chances = []
         for son in self.sons:
             if son.visits == 0:
@@ -124,34 +136,41 @@ class Node():
         else:
             return argmin(chances)
 
-    def player_chose(self, n):
+
+    def prob01(self):       #return probability of winning based on mcts
+        return f64(self.score)/self.visits
+
+    def player_chose(self, n):   #returns index of son player wants to go to based on index of line he intends to play
         for i in range(len(self.sons)):
             if (self.b.linije | n) == self.sons[i].b.linije:
                 return i
 
-    def mcts(self, n):
-        while (not self.done) and (n>0):
-            self.monte()
-            n-=1
-
-    def endlessMonte(self):
+    def endlessMonte(self):   #when playing against human, for pondering
         try:
             while True:
                 self.monte()
         except KeyboardInterrupt:
             pass
 
-    def fitModel(self, game_score, est_next):
-        y = 0
-        if self.done:
-            y = array(self.true_eval).reshape(1,1)
-        else:
-            y = f64(est_next + game_score)/2
-            y = array(y).reshape(1,1)
-        self.model.fit(self.inn, y, epochs=1, batch_size=1, verbose=0)
+    def fitModel(self, plus_one, plus_two):
+        y = 0                                #weights are updated in regard to final game result
+        if self.done:                        #and average of estimation two nodes after them in an
+            y = self.true_eval               #attempt to smoothen the graph of estimated values
+        else:                                #during the game
+            y = f64(plus_one + plus_two)/2                    #weights are also updated based on true eval when possible
+
+        self.data_in.append(self.b.getNumpyArray())
+        self.exp_data.append([y])
         if self.father == None:
+            self.model.fit(array(self.data_in), array(self.exp_data), epochs=1, verbose=0)
+            self.exp_data = []
+            self.data_in = []
             print('weights updated')
             return
-        self.father.fitModel(game_score, self.estimate)
+        for brother in self.father.sons:        #also, weights are trained to minimize the difference between
+            if brother is not self:             #estimated probabilities and probabilities calculated by mtcs
+                self.data_in.append(brother.b.getNumpyArray())
+                self.exp_data.append([brother.prob01()])
+        self.father.fitModel(self.estimate, y)
 
 
